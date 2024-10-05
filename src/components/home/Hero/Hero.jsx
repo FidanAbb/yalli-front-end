a51faf3ab273aeabdc10e9ff75a566b75553eade
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-10-05 06:43:52 +0400

    text

--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./style.module.scss";
 import heroBackk from "../../../assets/img/heroBackk.svg";
 import Arrow from "../../ui/Arrow";
@@ -11,17 +11,36 @@ const texts = [
   "Ölkəni seç, orada yaşayan azərbaycanlılarla asanlıqla tanış ol!",
   "Maraq dairəni və şəhəri qeyd et, müvafiq icmalarla tanış ol!",
 ];
+const countryCategory = [
+  "Polşa",
+  "Almaniya",
+  "Amerika",
+  "Kanada",
+  "Avstraliya",
+  "İngiltərə",
+  "Fransa",
+  "İspaniya",
+  "İtaliya",
+  "Çin",
+  "Hindistan",
+  "Rusiya",
+  "Qazaxıstan",
+];
 const Hero = () => {
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [selectedCountry, setSelectedCountry] = useState(""); 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTextIndex((prevIndex) =>
         prevIndex === texts.length - 1 ? 0 : prevIndex + 1
       );
-    }, 6000); 
+    }, 6000);
 
     return () => clearInterval(intervalId);
   }, []);
+  const handleSelectChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
   return (
     <div className={styles["hero"]}>
       <div className={styles["hero_left_circle"]}>
@@ -42,22 +61,32 @@ const Hero = () => {
           <div className={styles["find_box"]}>
             <h2>Yaşadığın ölkədə yerlilərini tap</h2>
             <div className={styles["find_inp"]}>
-            <div className={styles["texts"]}>
-                <p>{texts[currentTextIndex]}</p>
-              </div>
+            {selectedCountry === "" && (
+                <div className={styles["texts"]}>
+                  <p>{texts[currentTextIndex]}</p>
+                </div>
+              )}
               <div className={styles["earthicon"]}>
                 <EarthIcon />
               </div>
 
-              <select name="" id="">
-                <option value=""></option>
-                <option value="">ENG</option>
+              <select
+                value={selectedCountry}
+                onChange={handleSelectChange} 
+              >
+                <option value="" disabled>
+                  
+                </option>
+                {countryCategory.map((c, i) => (
+                  <option value={c} key={i}>
+                    {c}
+                  </option>
+                ))}
               </select>
 
               <div className={styles["down_arrow"]}>
                 <DownArrow />
               </div>
-
             </div>
           </div>
           <div className={styles["ellipsis"]}>
