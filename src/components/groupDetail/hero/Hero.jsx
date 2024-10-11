441b63e73a2e55db18b017bff05de870efd0f732
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-10-12 00:07:49 +0400

    detail

--- src/components/groupDetail/hero/Hero.jsx
+++ src/components/groupDetail/hero/Hero.jsx
@@ -3,8 +3,8 @@ import styles from "./style.module.scss";
 import JoinGroupIcon from "../../ui/JoinGroupIcon";
 import ShareIcon from "../../ui/ShareIcon";
 import GroupIcon from "../../ui/GroupIcon";
-import HeroImg from "../../../assets/img/groupDetail.svg"
-const Hero = ({group}) => {
+import HeroImg from "../../../assets/img/groupDetail.svg";
+const Hero = ({ group }) => {
   return (
     <div className={styles["hero"]}>
       <div className="container">
@@ -17,14 +17,22 @@ const Hero = ({group}) => {
               </button>
             </div>
             <div className={styles["down"]}>
-                <img src={`https://minio-server-4oyt.onrender.com/yalli/${group?.imageId}`} alt="" />
-                <div className={styles["detail"]}>
-                    <p>{group.description}</p>
+              <img
+                src={`https://minio-server-4oyt.onrender.com/yalli/${group?.imageId}`}
+                alt=""
+              />
+              <div className={styles["detail"]}>
+                <p>{group.description}</p>
                 <div className={styles["foot"]}>
-                    <p><GroupIcon color="#111111"/>{group.memberCount}+ üzv</p>
-                    <ShareIcon/>
+                  <p>
+                    <GroupIcon color="#111111" />
+                    {group.memberCount}+ üzv
+                  </p>
+                  <div>
+                    <ShareIcon />
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
           </div>
         </div>
