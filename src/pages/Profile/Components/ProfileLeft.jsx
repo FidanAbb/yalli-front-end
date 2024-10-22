1cd424ee299c875a0a116bf429b63f78417327c2
Elxan <elxanxanlarov@egmail.com>
2024-10-22 23:40:06 +0400

    Update of Elkhan

--- src/pages/Profile/Components/ProfileLeft.jsx
+++ src/pages/Profile/Components/ProfileLeft.jsx
@@ -1,8 +1,10 @@
 import { FaRegUserCircle } from "react-icons/fa";
 import { IoIosNotificationsOutline } from "react-icons/io";
-import { IoNotificationsOutline } from "react-icons/io5";
+import { IoNotificationsOutline, IoSettingsOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
-import mentorIcon from "../../../../src/pages/Profile/assets/img/mentor-icon.svg"
+import mentorIcon from "../../../../src/pages/Profile/assets/img/mentor-icon.svg";
+import { GoPeople } from "react-icons/go";
+import { CiCircleQuestion } from "react-icons/ci";
 const ProfileLeft = () => {
   return (
     <div className="col-md-6 col-sm-6 col-12">
@@ -11,7 +13,7 @@ const ProfileLeft = () => {
           <ul>
             <li>
               <Link className="link" to="/profile/profile-info">
-                <FaRegUserCircle className="user-icon"/>
+                <FaRegUserCircle className="user-icon" />
                 Profil Məlumatları
               </Link>
             </li>
@@ -23,17 +25,25 @@ const ProfileLeft = () => {
             </li>
             <li>
               <Link className="link" to="/profile/profile-mentoring">
-              <img src={mentorIcon} className="mentor-icon" alt="" />
+                <img src={mentorIcon} className="mentor-icon" alt="" />
                 Mentoring
               </Link>
             </li>
             <li>
               <Link className="link" to="/profile/profile-community-edit">
-                Community Edit
+                <GoPeople />
+                Community Edi
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/profile/profile-settings">
+              <IoSettingsOutline />
+                Parametrlər
               </Link>
             </li>
             <li>
               <Link className="link" to="/profile/profile-help">
+              <CiCircleQuestion className="help-icon" />
                 Help
               </Link>
             </li>
